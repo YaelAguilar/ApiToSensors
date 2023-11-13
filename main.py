5cbe7b37c1b0a767b06674e31abf8817006fb67c
YaelAguilar <213452@ids.upchiapas.edu.mx>
2023-11-12 22:02:40 -0600

    [ADD] routes for sensors

--- main.py
+++ main.py
@@ -6,3 +6,18 @@ app = FastAPI()
 def index():
     return "api is running"
 
+@app.get("/BPM")
+def BPM():
+    return "BPM: 92"
+
+@app.get("/oxygenation")
+def oxygenation():
+    return "Oxygenation: 100%"
+
+@app.get("/temperature")
+def temperature():
+    return"Temperature: 37°C"
+
+@app.get("/physicactivity")
+def physicactivity():
+    return "PhysicActivity: high"
